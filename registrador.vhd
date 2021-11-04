7817b6efb0674b605df48c6e8cd53fb93a085908
Gustavo Guedes <gustavoipguedes@gmail.com>
2021-11-04 19:48:00 -0300

    feat: Lab #4 Registrador Completo

---
+++ registrador.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity registrador is
+    port(   
+        clk         :   in std_logic;
+        rst         :   in std_logic;
+        wr_en       :   in std_logic;
+        data_in     :   in  unsigned(15 downto 0);
+        data_out    :   out unsigned(15 downto 0)
+    );
+end entity registrador;
+
+
+architecture a_registrador of registrador is 
+    signal registro     :   unsigned(15 downto 0);
+begin 
+    process(clk,rst,wr_en)
+    begin 
+        if rst='1' then 
+            registro <= "0000000000000000"; --reset
+        elsif wr_en='1' then
+            if rising_edge(clk) then 
+                registro <= data_in; --set
+            end if;
+        end if;
+    end process;
+
+    data_out <= registro;
+end architecture;
